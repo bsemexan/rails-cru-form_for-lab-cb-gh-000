8cb0a706f77e9ce669abe1979e80b11465bc58e0
Berry  Semexan <bsemexan@gmail.com>
2018-03-26 17:18:12 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,32 @@
 class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(artist_params)
+	  @genre.save
+	  redirect_to artist_path(@genre)
+  end
+
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+    def update
+      @artist = Artist.find(params[:id])
+  	  @artist.update(artist_params)
+  	  redirect_to artist_path(@artist)
+    end
+
+    private
+
+    def genre_params
+			params.require(:artist).permit(:name, :bio)
+		end
 end
