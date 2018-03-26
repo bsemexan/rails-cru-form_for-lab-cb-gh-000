3628769e8bebe4e85106d3a978ded660883510af
Berry  Semexan <bsemexan@gmail.com>
2018-03-26 16:27:59 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,20 +2,25 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
   def create
     @artist = Artist.new(artist_params)
 	  @artist.save
 	  redirect_to artist_path(@artist)
   end
 
-  def update
-  end
 
-  def edit 
+  def edit
     @artist = Artist.find(params[:id])
   end
 
   def show
     @artist = Artist.find(params[:id])
   end
+
+    def update
+      @artist = Artist.find(params[:id])
+  	  @artist.update(artist_params)
+  	  redirect_to artist_path(@artist)
+    end
 end
