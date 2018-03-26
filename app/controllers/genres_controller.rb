093cf3a035b4fe1a506af5b60f33f5a86cb1258a
Berry  Semexan <bsemexan@gmail.com>
2018-03-26 17:52:35 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -8,7 +8,7 @@ class GenresController < ApplicationController
   end
 
   def create
-    @genre = Genre.new(artist_params)
+    @genre = Genre.new(genre_params)
 	  @genre.save
 	  redirect_to genre_path(@genre)
   end
